0e8bce37ea477963eae4244888b69119cfee67ce
Linda Masia <masialr3x@gmail.com>
2025-04-06 22:04:50 +0200

    calculator test

---
+++ calculator/src/app/calculator/page.tsx
@@ -0,0 +1,9 @@
+import Calculator from "@/components/Calculator"
+
+export default function CalculatorPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Calculator />
+    </div>
+  )
+}
